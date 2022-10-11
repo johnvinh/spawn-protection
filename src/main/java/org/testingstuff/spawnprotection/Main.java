de0d9fdf6daed402d4bc38161c0be0d34d97b9a4
John Vinh <john.michael.vinh2@gmail.com>
2022-10-10 20:42:07 -0400

    Initial commit

---
+++ src/main/java/org/testingstuff/spawnprotection/Main.java
@@ -0,0 +1,47 @@
+package org.testingstuff.spawnprotection;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        if (isInProtectedArea(e))
+            e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if (isInProtectedArea(e))
+            e.setCancelled(true);
+    }
+
+    public boolean isInProtectedArea(BlockEvent e) {
+        int x = e.getBlock().getLocation().getBlockX();
+        int z = e.getBlock().getLocation().getBlockZ();
+        int xStart = 11;
+        int xEnd = 15;
+        int zStart = -78;
+        int zEnd = -82;
+        if (x >= xStart && x <= xEnd)
+            return z >= zEnd && z <= zStart;
+        return false;
+    }
+}
