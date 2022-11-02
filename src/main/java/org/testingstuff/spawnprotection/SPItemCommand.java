60efc73b0f896425e1353287868bc7ab8ef30d76
John Vinh <john.michael.vinh2@gmail.com>
2022-11-02 14:45:36 -0400

    Add item to player's inventory when command is used

--- src/main/java/org/testingstuff/spawnprotection/SPItemCommand.java
+++ src/main/java/org/testingstuff/spawnprotection/SPItemCommand.java
@@ -1,9 +1,12 @@
 package org.testingstuff.spawnprotection;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class SPItemCommand implements CommandExecutor {
     @Override
@@ -11,6 +14,11 @@ public class SPItemCommand implements CommandExecutor {
         if (!(sender instanceof Player)) {
             return false;
         }
+        ItemStack is = new ItemStack(Material.WOODEN_HOE);
+        ItemMeta meta = is.getItemMeta();
+        meta.setDisplayName("Set Spawn Protect");
+        is.setItemMeta(meta);
+        ((Player) sender).getInventory().addItem(is);
         return true;
     }
 }
